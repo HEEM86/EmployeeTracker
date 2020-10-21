be01ebce1295a31a9948f8ea3b4fa701447a2d80
Slice Wess <ernestwesson86@gmail.com>
2020-10-21 00:00:01 -0400

    updating application and readme

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# EmployeeTracker
+# Employee Tracker 
+
+
+**Description**
+
+The Employee Tracker allows the user to go into the database to view, add or make changes. 
+
+Features:
+
+- User has the ability to View all Departments and Employees
+- User has the ability to Vill all of the active Employees
+- User has the ability to make changes to database: add/remove
+
+
+## The Finished Product 
+
+
+![Database Work]("https://i.ibb.co/FbJk6TJ/database-work.png>)
+
+
+
+## Challenges :loudspeaker:
+
+I was not successfull with completing the Employee Tracker Database. Here are the following challenges I faced.
+
+1. Trying to find the right functions for the operations I needed. The addDepartment function is something I just couldn't figure out. I tried a variety of functions from the previous activities and homework assingments but nothing worked
+
+2. Mysql workbench was difficult trying to set up the server to connect with my applicaton 
+
+3. Wasn't able to get any of the other areas working....
+
+4. Being confused, with the material and lacking understanding. 
+
+## Author :zap:
+
+Ernest Wesson 
+
+
+## Credits :star:
+
+Slack: Study Group #3
+
+Lofi Music: https://www.youtube.com/watch?v=tutZKLeGrCs
+
+
+## License :page_with_curl:
+
+
+MIT License
+
+Copyright (c) [2020] [Ernest]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+https://choosealicense.com/licenses/mit/
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+const { connect } = require("http2");
 
 
 var connection = mysql.createConnection({
@@ -13,12 +14,228 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "Slice1986!",
-  database: "ice_creamDB"
+  database: "employees_db"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
-  createProduct();
-  
+  beginSearch();
 });
+
+function beginSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Add New Deaprtment",
+        "View Departments",
+        "Add Employee",
+        "View Employees",
+        "View all Employess by Manager",
+        "Update Employee Role",
+        "Exit"
+      ]
+    })
+    
+   .then(answers => {
+
+    switch(answers.beginSearch){
+
+      case "Add New Deaprtment" : addDepartment(); 
+      break;
+
+      case "View Departments" : viewDepartment(); 
+      break;
+
+      case "Add Employee" : addEmployee();
+      break;
+
+      case "View Employees" : viewEmployee();
+      break;
+
+      case "Update Employee Role" : updateEmployee();
+      break;
+
+      Exit: 
+      console.log("Goodbye, see you next time!");
+      connection.exit(); 
+
+   };
+   });
+  };
+
+  function addDepartment() {
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "department",
+            message: "Add Department: "
+        }
+    ]).then(answers=> {
+        
+        connection.query(
+            "INSERT INTO department SET ?",
+            {
+              department_name: answers.department,
+            },
+            function(err) {
+              if (err) throw err;
+              console.log("New Department added successfully");
+              // re-prompt the user 
+              addDepartment();
+            }
+          );
+    }); 
+};
+
+
+function viewDepartment() {
+  var query = "SELECT * FROM department";
+    connection.query(query, function(err, res) {
+        console.log(`DEPARTMENTS:`)
+      res.forEach(department => {
+          console.log(`ID: ${department.id} | Name: ${department.name}`)
+      })
+      viewDepartment();
+      });
+  };
+
+
+  
+
+// function artistSearch() {
+//   inquirer
+//     .prompt({
+//       name: "artist",
+//       type: "input",
+//       message: "What artist would you like to search for?"
+//     })
+//     .then(function(answer) {
+//       var query = "SELECT position, song, year FROM top5000 WHERE ?";
+//       connection.query(query, { artist: answer.artist }, function(err, res) {
+//         for (var i = 0; i < res.length; i++) {
+//           console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+//         }
+//         runSearch();
+//       });
+//     });
+// }
+
+// function multiSearch() {
+//   var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
+//   connection.query(query, function(err, res) {
+//     for (var i = 0; i < res.length; i++) {
+//       console.log(res[i].artist);
+//     }
+//     runSearch();
+//   });
+// }
+
+// function rangeSearch() {
+//   inquirer
+//     .prompt([
+//       {
+//         name: "start",
+//         type: "input",
+//         message: "Enter starting position: ",
+//         validate: function(value) {
+//           if (isNaN(value) === false) {
+//             return true;
+//           }
+//           return false;
+//         }
+//       },
+//       {
+//         name: "end",
+//         type: "input",
+//         message: "Enter ending position: ",
+//         validate: function(value) {
+//           if (isNaN(value) === false) {
+//             return true;
+//           }
+//           return false;
+//         }
+//       }
+//     ])
+//     .then(function(answer) {
+//       var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
+//       connection.query(query, [answer.start, answer.end], function(err, res) {
+//         for (var i = 0; i < res.length; i++) {
+//           console.log(
+//             "Position: " +
+//               res[i].position +
+//               " || Song: " +
+//               res[i].song +
+//               " || Artist: " +
+//               res[i].artist +
+//               " || Year: " +
+//               res[i].year
+//           );
+//         }
+//         runSearch();
+//       });
+//     });
+// }
+
+// function songSearch() {
+//   inquirer
+//     .prompt({
+//       name: "song",
+//       type: "input",
+//       message: "What song would you like to look for?"
+//     })
+//     .then(function(answer) {
+//       console.log(answer.song);
+//       connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
+//         console.log(
+//           "Position: " +
+//             res[0].position +
+//             " || Song: " +
+//             res[0].song +
+//             " || Artist: " +
+//             res[0].artist +
+//             " || Year: " +
+//             res[0].year
+//         );
+//         runSearch();
+//       });
+//     });
+// }
+
+// function songAndAlbumSearch() {
+//   inquirer
+//     .prompt({
+//       name: "artist",
+//       type: "input",
+//       message: "What artist would you like to search for?"
+//     })
+//     .then(function(answer) {
+//       var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
+//       query += "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
+//       query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
+
+//       connection.query(query, [answer.artist, answer.artist], function(err, res) {
+//         console.log(res.length + " matches found!");
+//         for (var i = 0; i < res.length; i++) {
+//           console.log(
+//             i+1 + ".) " +
+//               "Year: " +
+//               res[i].year +
+//               " Album Position: " +
+//               res[i].position +
+//               " || Artist: " +
+//               res[i].artist +
+//               " || Song: " +
+//               res[i].song +
+//               " || Album: " +
+//               res[i].album
+//           );
+//         }
+
+//         runSearch();
+//       });
+  
